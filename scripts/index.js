b5de4a0feed31ae3255d4e8c190dbd264aef37b5
Amairani Romero <12romeroamairani@gmail.com>
2026-02-16 13:04:32 -0600

    add events

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,48 @@ let initialCards = [
 initialCards.forEach(function (item) {
   console.log(item.name);
 });
+
+let openEditModal = document.querySelector(".profile__edit-button");
+let editModal = document.querySelector("#edit-popup");
+let closeEditModal = editModal.querySelector(".popup__close");
+let profileTitle = document.querySelector(".profile__title");
+let profileDescription = document.querySelector(".profile__description");
+let nameInput = editModal.querySelector(".popup__input_type_name");
+let descriptionInput = editModal.querySelector(
+  ".popup__input_type_description",
+);
+//Formulario
+let formElement = editModal.querySelector(".popup__form");
+
+function openModal(modalElement) {
+  modalElement.classList.add("popup_is-opened");
+}
+
+function closeModal(modalElement) {
+  modalElement.classList.remove("popup_is-opened");
+}
+
+openEditModal.addEventListener("click", handleOpenEditModal);
+
+closeEditModal.addEventListener("click", handleProfileFormSubmit);
+
+function fillProfileForm() {
+  nameInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editModal);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  closeModal(editModal);
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
